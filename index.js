a7d86cdd0ce0b5fa0b239e4dfd5d62d3c3de4bba
jiiaa <jukkaa@live.com>
2022-04-03 17:05:49 +0300

    Exercise 13.3. with sequelize model

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes, QueryTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialectOptions: {
@@ -10,12 +10,42 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   },
 });
 
+class Blog extends Model{};
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+   url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+});
+
 const main = async () => {
   try {
     await sequelize.authenticate();
-    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
+    //const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
+    const blogs = await Blog.findAll();
     blogs.forEach(element => {
-      console.log('%s: \'%s\', %s likes ', element.author, element.title, element.likes);
+      console.log('%s: %c\'%s\'%c, %s likes ', element.author, 'color: #bde052;', element.title, 'color: white;', element.likes);
     });
     sequelize.close();
   } catch (err) {
