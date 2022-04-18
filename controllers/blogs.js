f574f36be31782021ee278fe70370e0fd09a8d3c
jiiaa <jukkaa@live.com>
2022-04-18 11:54:52 +0300

    Exercise 13.14. ready

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -32,13 +32,24 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const where = {};
+  let where = {};
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
-    };
-  }
+    where = {
+      [Op.or]: [
+        {
+          author: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        },
+        {
+          title: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        }
+      ]
+    }
+  };
 
   try {
     const blogs = await Blog.findAll({
