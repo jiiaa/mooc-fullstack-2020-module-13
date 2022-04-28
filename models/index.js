fda826120fa28c5dc3696c3581d8c0e62dc958df
jiiaa <jukkaa@live.com>
2022-04-28 20:22:03 +0300

    Exercise 13.19. ready

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 const Blog = require('./blog');
 const User = require('./user');
+const ReadList = require('./read_list');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-module.exports = { Blog, User };
+User.belongsToMany(Blog, {through: ReadList, as: 'readBlogs' });
+Blog.belongsToMany(User, {through: ReadList, as: 'userRead' });
+
+
+module.exports = { Blog, User, ReadList };
